454e7b8d7fe2d4c17d9b67e83c437df312765713
PgLESv <81176654+PgLESv@users.noreply.github.com>
2022-06-15 14:58:45 -0300

    Testes

---
+++ TestMSTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetoTesteFST1;
+
+namespace TestMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SomarDoisNumeros()
+        {
+            //Arrange - Preparação
+            double pNum = 1;
+            double sNum = 1;
+            double rNum = 2;
+
+            //Act - Ação
+            var resultado = Operacoes.Somar(pNum, sNum);
+
+            //Assert - Verificação
+            Assert.AreEqual(rNum, resultado);
+        }
+
+
+
+        [DataTestMethod]
+        [DataRow(1, 1, 2)]
+        [DataRow(2, 2, 4)]
+        [DataRow(2, 1, 2)]
+        public void SomarDoisNumerosLista(double pNum, double Snum, double rNum)
+        {
+            //Act - Ação
+            var resultado = Operacoes.Somar(pNum, Snum);
+
+            //Assert - Verificação
+            Assert.AreEqual(rNum, resultado);
+        }
+
+    }
+}
